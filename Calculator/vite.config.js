bc23179bc26bc4b444eb3fc849671cb8013aa13d
Sheikh Mushfiqur Rakib <smushfiqurrakib@gmail.com>
2026-06-18 15:30:53 +0600

    Changed port number

--- Calculator/vite.config.js
+++ Calculator/vite.config.js
@@ -3,9 +3,14 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-  ]
+  ],
+  server: {
+    port: 3000, // Replace 3000 with your preferred port number
+    strictPort: true, // Optional: Force Vite to exit if the port is already in use
+  }
 })
